7c563e57086e1eb5932f5de461dc6cec4f705897
ANDRE_Ani <andreani.patrice@net-c.fr>
2020-11-13 16:50:25 +0100

    Modifications design

--- content-single.php
+++ content-single.php
@@ -13,7 +13,7 @@
                 <path fill-rule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
                 <path fill-rule="evenodd" d="M2 15v-1c0-1 1-4 6-4s6 3 6 4v1H2zm6-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
             </svg>
-            <a href="#"><?php the_author(); ?></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <?php the_author(); ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-folder" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M9.828 4a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 6.173 2H2.5a1 1 0 0 0-1 .981L1.546 4h-1L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3v1z" />
--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         </div>
 
 
-        <div class="col-sm-4">
+        <div class="col-sm-4 side">
             <?php get_sidebar(); ?>
         </div>
 
